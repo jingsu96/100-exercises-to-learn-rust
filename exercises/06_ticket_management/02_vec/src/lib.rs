31e49f385aeab377bf49450da48f660ea1dcfbf6
JingHuangSu1996 <jing.tech.tw@gmail.com>
2024-07-28 22:15:19 +0800

    chore: ch6.2

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -10,12 +10,24 @@
 //
 // We expect `fibonacci(0)` to return `0`, `fibonacci(1)` to return `1`,
 // `fibonacci(2)` to return `1`, and so on.
+
 pub fn fibonacci(n: u32) -> u32 {
     // TODO: implement the `fibonacci` function
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut cached: Vec<u32> = vec![0, 1];
+
+    if n < cached.len() as u32 {
+        return cached[n as usize];
+    }
+
+    for i in cached.len() as u32..=n {
+        let next = cached[(i - 1) as usize] + cached[(i - 2) as usize];
+        cached.push(next);
+    }
+
+    return cached[n as usize];
 }
 
 #[cfg(test)]
